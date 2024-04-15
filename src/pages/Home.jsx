75ea621015d63aa02ea7a0e674db6db0eb0ee0cd
gabrielpires11 <gabri4590@gmail.com>
2024-04-15 17:25:19 -0300

    Pires

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../App.css';
+import './Home.css';
+import Logo from './img/Logo_Senai.jpeg';
 
 function Home() {
     return (
         <>
-            <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Login</Nav.Link>
-                        <Nav.Link href="/home">Home</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+            <div className='Card-Nav'>
+                <Navbar bg="dark" data-bs-theme="dark">
+                    <Container>
+                        <Navbar.Brand href="#home">
+                            <img src={Logo} alt="logosenai" width='200px' />
+                        </Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Login</Nav.Link>
+                            <Nav.Link href="/home">Home</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </div>
         </>
-        
-        
+
+
     );
 }
 
--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,34 +1,50 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './LoginForm.css';
-import './img/Logo_Senai.jpeg';
+import Logo from './img/Logo_Senai.jpeg';
 
 function LoginForm() {
     return (
-        <div className="login">
-            <form className="needs-validation">
-                <div className="form-group was-validated mb-2">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" required></input>
-                    <div className="invalid-feedback">
-                        Por favor insira seu email
-                    </div>
-                </div>
-                <div className="form-group was-validated mb-2">
-                    <label htmlFor="password" className="form-label">Senha</label>
-                    <input type="password" className="form-control" required></input>
-                    <div className="invalid-feedback">
-                        Por favor insira sua senha
-                    </div>
-                </div>
-                <div className="form-group form-check mb-2">
-                    <label htmlFor="check" className="form-check-label">Lembrar de mim</label>
-                    <input type="checkbox" className="form-check-input"></input>
-                </div>
-                <button type="submit" id="btn-enviar" className="btn btn-sucess block mt-2">Enviar</button>
-                <button type="submit" id="btn-cadastrar" className="btn btn-sucess block mt-2">Cadastrar</button>
-            </form>
+        <>
+        <div class="img">
+        <img src={Logo} alt="logosenai" width="400px"/>
         </div>
+
+            <div className="cardGeral">
+
+                <div class='texto'>
+                    <h1>Bem-vindo</h1>
+                    <p>Este Carômetro vai te ajudar a se autoconhecer<br />e a connhecer a seus amigos de outras salas.<br />Seja bem-vindo à jornada de autoconhecimento<br />guiada pelos princípios do SENAI.</p>
+                </div>
+
+                <div className="login">
+                    <form className="needs-validation">
+                        <div className="form-group was-validated mb-2">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" className="form-control" required></input>
+                            <div className="invalid-feedback">
+                                Por favor insira seu email
+                            </div>
+                        </div>
+                        <div className="form-group was-validated mb-2">
+                            <label htmlFor="password" className="form-label">Senha</label>
+                            <input type="password" className="form-control" required></input>
+                            <div className="invalid-feedback">
+                                Por favor insira sua senha
+                            </div>
+                        </div>
+                        <div className="form-group form-check mb-2">
+                            <label htmlFor="check" className="form-check-label">Lembrar de mim</label>
+                            <input type="checkbox" className="form-check-input"></input>
+                        </div>
+                        <button type="submit" id="btn-enviar" className="btn btn-sucess block mt-2">Enviar</button>
+                        <button type="submit" id="btn-cadastrar" className="btn btn-sucess block mt-2">Cadastrar</button>
+                    </form>
+                </div>
+
+            </div>
+
+        </>
     )
 }
 
